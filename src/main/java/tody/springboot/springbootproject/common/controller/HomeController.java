d134fba05f1b35983c29dc613c6ea9351b651934
Doyun <y0unn@hanmail.net>
2019-11-29 13:10:47 +0900

    Refactor: create common package

--- src/main/java/tody/springboot/springbootproject/common/controller/HomeController.java
+++ src/main/java/tody/springboot/springbootproject/common/controller/HomeController.java
@@ -1,4 +1,4 @@
-package tody.springboot.springbootproject;
+package tody.springboot.springbootproject.common.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
