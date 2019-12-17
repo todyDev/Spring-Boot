4985d3916d1a7c138bb995b8798f535591e0b1ed
Doyun <y0unn@hanmail.net>
2019-12-17 10:39:47 +0900

    add: argument resolver bean

--- src/main/java/tody/springboot/springbootproject/configuration/WebConfig.java
+++ src/main/java/tody/springboot/springbootproject/configuration/WebConfig.java
@@ -1,10 +1,14 @@
 package tody.springboot.springbootproject.configuration;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import tody.springboot.springbootproject.interceptor.CommonInterceptor;
+import tody.springboot.springbootproject.resolver.CustomMapArgumentResolver;
 
 /**
  * WebConfig
@@ -16,6 +20,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new CommonInterceptor()).addPathPatterns("/**");
         WebMvcConfigurer.super.addInterceptors(registry);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        WebMvcConfigurer.super.addArgumentResolvers(resolvers);
+        resolvers.add(new CustomMapArgumentResolver());
     }
 
 }
