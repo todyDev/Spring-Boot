32ad6f41720112c5123bd308dad4a15c609fd2d6
Doyun <y0unn@hanmail.net>
2019-12-18 12:03:32 +0900

    라우터 등록 - 게시판 목록

--- vuejs/src/router/index.js
+++ vuejs/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import BoardList from '@/components/board/BoardList'
 
 Vue.use(Router)
 
@@ -10,6 +11,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/board/boardList',
+      name: 'BOARD-LIST-PAGE',
+      component: BoardList
     }
+
   ]
 })
